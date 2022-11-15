91fe9ff96866cd4f9a99aae3c666d337e62fd036
tyreyryr <81716468+tyreyryr@users.noreply.github.com>
2022-11-15 14:33:46 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,21 @@
+function validate() {
+    var username = document.getElementById("Username").value;
+    var passowrd = document.getElementById("Password").value;
+    var ErrorMessage = document.getElementById("Error")
+    var SuccessfulMessage = document.getElementById("Successfuly")
+    if (username=="admin"&& passowrd=="admin") {
+        SuccessfulMessage.style.opacity=1;
+        setTimeout(function () {
+            window.open("https://www.roblox.com/home");
+            SuccessfulMessage.style.opacity=0;
+        }, 1000);
+        return false;
+    }else{
+        ErrorMessage.style.opacity=1;
+        setTimeout(function () {
+            ErrorMessage.style.opacity=0;
+        }, 1000);
+        
+    }
+
+}
